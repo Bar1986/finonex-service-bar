3250ff198f5e11d9f4e11c43e481447240e2b901
Bar Feldman <barfeldman1986@gmail.com>
2024-10-05 04:48:28 +0300

    finished retrive  data from db

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-import express from "express";
+import dotenv from 'dotenv'
+dotenv.config();
+
+import express from 'express';
+import * as fs from 'fs';
+import postgres from 'pg'
+
+
+
+const pool = new postgres.Pool({
+    user:  process.env.DB_USER  ||  'admin',
+    host: process.env.DB_HOST   || 'localhost',
+    database: process.env.DB_NAME || 'finonex',
+    password: process.env.DB_PASSWORD || '123456',
+    port: process.env.DB_PORT || 5432,
+})
 
 const app = express();
 const port = process.env.PORT || 8000;
+const filePath = process.env.FILE_PATH || './users_transaction.json';
+
 app.use(express.json());
+
+app.use((err,req,res,next)  => {
+    res.status(err.response ? err.response.status : 500).json({error: err.message});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-  
+ 
 
+const handleErrorAsync = func => async (req,res,next) => {
+    try {
+        await func(req,res,next)
+    } catch(error) {
+        next(error)
+    }
+}
 const customAuthentication = (header) => (req, res, next) => {
     if (req.headers['authorization'] !== header) {
       return res.status(401).send('Unauthorized!');
@@ -16,13 +44,32 @@ const customAuthentication = (header) => (req, res, next) => {
     next();
   };
 
-app.post('/liveEvent',customAuthentication('secret'), (req, res) => {
-  res.send('Welcome to the root route');
+app.post('/liveEvent',customAuthentication('secret'), (req, res) => {   
+    appendToFile(req.body)
+    res.send({message: 'event  added to file'});
 });
-app.get('/userEvents/:userid',customAuthentication('secret'), (req, res) => {
-    res.send({user : req.params.userid});
-  });
+app.get('/userEvents/:userid',customAuthentication('secret'),handleErrorAsync( async (req, res) => {
+        const {userid} = req.params;
+       const response =  await retriveFromDB(userid)
+        res.status(200)
+        .send(response);
+}))  
 
+
+const appendToFile = (data) => {
+    if(fs.existsSync(filePath)) {
+       fs.appendFileSync(filePath, JSON.stringify(data) + '\n');
+       return;
+    }
+    fs.writeFileSync(filePath,JSON.stringify(data) + '\n');
+}
+
+const retriveFromDB = async (userId) => {
+        const db = await pool.connect();
+        const response = await db.query('SELECT * FROM users_revenue WHERE user_id = $1 ', [userId]);
+        db.release();
+        return response.rows.length > 0 ? response.rows[0] : {}
+}
 
 
 
